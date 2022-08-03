9a4febcf62b3730cf68c2a50fa46b66190b7373c
suzettecomia <suzettecomia25@gmail.com>
2022-08-03 22:10:38 +0800

    Update codes

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -10,7 +10,7 @@ export class FoodService {
             {
                 id:1,
                 title: 'RED VELVET',
-                price: 39,
+                price: 60,
                 image:'assets/images/red_velvet.png',
                 description:
                 'A red velvet cake made liquid with fragrant vanilla lightly scented with a hint of chocolate served with tapioca pearls slow cooked with Okinawan brown sugar and fresh milk.'
@@ -18,7 +18,7 @@ export class FoodService {
             {
                 id:2,
                 title: 'OREO',
-                price: 39,
+                price: 65,
                 image:'assets/images/oreo.png',
                 description:
                 'Pour tapioca pearls and milk tea into the cup, then pour cream topping and oreo crumbs on top of the drink.'
@@ -26,7 +26,7 @@ export class FoodService {
             {
                 id:3,
                 title: 'OKINAWA',
-                price: 39,
+                price: 60,
                 image:'assets/images/okinawa.png',
                 description:
                 'Deviating from traditional tapioca pearls, the Okinawa Boba Co. features crystal boba jelly made from plant based konjac which is both more consistent in texture and healthier than traditional tapioca.'
@@ -34,7 +34,7 @@ export class FoodService {
             {
                 id:4,
                 title: 'TARO',
-                price: 39,
+                price: 65,
                 image:'assets/images/taro.png',
                 description:
                 'It has a very sweet taste, with a slight hint of vanilla. In taro boba tea, there is sugar and milk added so it will taste much sweeter than eating the vegetable on its own.'
@@ -42,15 +42,15 @@ export class FoodService {
             {
                 id:5,
                 title: 'DARK CHOCOLATE',
-                price: 39,
+                price: 65,
                 image:'assets/images/dark_choco.png',
                 description:
                 'It tastes a bit like ice-cold hot chocolate, but with an extra surprise (boba pearls) at the bottom.'
             },
             {
                 id:6,
-                title: 'WINTERMELON',
-                price: 39,
+                title: 'WINTER MELON',
+                price: 60,
                 image:'assets/images/wintermelon.png',
                 description:
                 'Wintermelon Milk Tea are wintermelon syrup, milk, tea (or water) and ice cubes. Tapioca pearls can also be added to turn the drink into bubble tea.'
