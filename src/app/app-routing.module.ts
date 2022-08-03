da5872da1afd6ff8ab161d10ceb998ddaf598ddc
suzettecomia <suzettecomia25@gmail.com>
2022-08-03 10:53:24 +0800

    Add tabs to homepage file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
     path: '',
     redirectTo: 'welcome',
     pathMatch: 'full'
   },
-  
   {
     path: 'welcome',
     loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
-  },
-  {
-    path: 'listing',
-    loadChildren: () => import('./screens/listing/listing.module').then(m=> m.ListingPageModule)
   },
   {
     path: 'detail',
